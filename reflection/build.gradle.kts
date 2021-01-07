75ad34fc58febbd8c9619c07b666180a6b96c232
helloworlde <hehuimin03@meituan.com>
2021-01-07 16:00:39 +0800

    Add ReflectionService Sample

---
+++ reflection/build.gradle.kts
@@ -0,0 +1,53 @@
+import com.google.protobuf.gradle.*
+
+plugins {
+    java
+    idea
+    application
+    id("com.google.protobuf") version "0.8.14"
+    id("io.freefair.lombok") version "5.3.0"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+val grpcVersion = "1.34.1"
+val protocVersion = "3.12.0"
+val slf4jVersion = "1.7.25"
+
+dependencies {
+    implementation("io.grpc:grpc-netty:${grpcVersion}")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("io.grpc:grpc-services:${grpcVersion}")
+    implementation("org.slf4j:slf4j-api:${slf4jVersion}")
+    implementation("org.slf4j:slf4j-simple:${slf4jVersion}")
+
+    testImplementation("junit:junit:4.13")
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/resources/proto")
+        }
+    }
+}
+
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:${protocVersion}" }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
---
+++ reflection/src/main/java/io/github/helloworlde/grpc/ReflectionClient.java
@@ -0,0 +1,110 @@
+package io.github.helloworlde.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.reflection.v1alpha.ExtensionRequest;
+import io.grpc.reflection.v1alpha.ServerReflectionGrpc;
+import io.grpc.reflection.v1alpha.ServerReflectionRequest;
+import io.grpc.reflection.v1alpha.ServerReflectionResponse;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class ReflectionClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 构建 Channel
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("127.0.0.1", 9090)
+                                                      .usePlaintext()
+                                                      .build();
+
+        // 使用 Channel 构建 BlockingStub
+        ServerReflectionGrpc.ServerReflectionStub reflectionStub = ServerReflectionGrpc.newStub(channel);
+
+        StreamObserver<ServerReflectionResponse> streamObserver = new StreamObserver<ServerReflectionResponse>() {
+            @Override
+            public void onNext(ServerReflectionResponse response) {
+                log.info("{}", response);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("Complete");
+            }
+        };
+
+        StreamObserver<ServerReflectionRequest> requestStreamObserver = reflectionStub.serverReflectionInfo(streamObserver);
+
+        // 列举所有的服务
+        // 参考 reflection.proto:67#list_services
+        ServerReflectionRequest listServiceRequest = listService();
+        requestStreamObserver.onNext(listServiceRequest);
+
+        // 根据名称获取文件
+        // 参考 reflection.proto:44#file_by_filename
+        ServerReflectionRequest getFileByName = getFileByName("helloworld.proto");
+        requestStreamObserver.onNext(getFileByName);
+
+        // 根据符号获取服务定义
+        // 参考 reflection.proto:49#file_containing_symbol
+        ServerReflectionRequest getFileContainingSymbol = getFileContainingSymbol("io.github.helloworlde.grpc.HelloService");
+        requestStreamObserver.onNext(getFileContainingSymbol);
+
+
+        // 根据扩展名称和序号查询
+        // 参考 reflection.proto:53#file_containing_extension
+        // 是 annotation 中的扩展属性, extend 修饰的
+        ServerReflectionRequest fileContainingExtension = getServerReflectionRequest("google.protobuf.MethodOptions", 72295728);
+        requestStreamObserver.onNext(fileContainingExtension);
+
+        // 根据扩展名称查询
+        // 参考 reflection.proto:63#all_extension_numbers_of_type
+        ServerReflectionRequest allExtensionNumbersOfType = getAllExtensionNumbersOfType("google.protobuf.MethodOptions");
+        requestStreamObserver.onNext(allExtensionNumbersOfType);
+
+        requestStreamObserver.onCompleted();
+
+        // 等待终止
+        channel.awaitTermination(10, TimeUnit.SECONDS);
+    }
+
+    private static ServerReflectionRequest getAllExtensionNumbersOfType(String type) {
+        return ServerReflectionRequest.newBuilder()
+                                      .setAllExtensionNumbersOfType(type)
+                                      .build();
+    }
+
+    private static ServerReflectionRequest getFileContainingSymbol(String symbol) {
+        return ServerReflectionRequest.newBuilder()
+                                      .setFileContainingSymbol(symbol)
+                                      .build();
+    }
+
+    private static ServerReflectionRequest getServerReflectionRequest(String type, int number) {
+        return ServerReflectionRequest.newBuilder()
+                                      .setFileContainingExtension(ExtensionRequest.newBuilder()
+                                                                                  .setContainingType(type)
+                                                                                  .setExtensionNumber(number)
+                                                                                  .build())
+                                      .build();
+    }
+
+    private static ServerReflectionRequest getFileByName(String name) {
+        return ServerReflectionRequest.newBuilder()
+                                      .setFileByFilename(name)
+                                      .build();
+    }
+
+    private static ServerReflectionRequest listService() {
+        // 构建消息
+        return ServerReflectionRequest.newBuilder()
+                                      .build();
+    }
+}
